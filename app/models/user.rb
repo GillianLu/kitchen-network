b37c3e5a5679ecd05600bd7c066aab6be91feaf1
GillianLu <lu.gillian.nicole@gmail.com>
2024-05-21 16:07:49 +0800

    User & Role models

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  belongs_to :role
+  validates :first_name, :last_name, :email, :role_id, presence: true
+end

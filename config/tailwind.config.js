09fd40c1ab995711af699c4d81415d853fc90715
dhanniela <tjmaureadhanniela@gmail.com>
2024-05-27 21:18:25 +0800

    feat: Add styles

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,20 +5,22 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        lightdark: "#84848C",
-        mediumdark: "#8C8C93",
-        strongdark: "#15151D",
+        mediumgray: "#84848C",
+        stronggray: "#6A6D69", /*text*/
+        mediumgreen: '#48683eb9', /*hover*/
+        stronggreen: "#22331D", /*sidebar*/
         lightorange: "#F8BC7C",
         mediumorange: "#F7A465",
-        darkorange: "#FB730C",
+        darkorange: "#F65A11", /*buttons & active*/
+        linen: "#EFEDE7", /*background*/
       },
     },
   },
@@ -26,5 +28,10 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+  ],
+  variants: {
+    extend: {
+      borderWidth: ['hover', 'focus'],
+    },
+  },
 }

9f7f9fc67e288ce94af2b8a1772a6f46b5ca8338
mrkiyn <amado.markian@gmail.com>
2024-05-29 03:26:03 +0800

    Finish partial send payment.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,91 +1,49 @@
 class PaymentsController < ApplicationController
-  def new
-    @job_listing = JobListing.find(params[:job_listing_id])
-    @applied_job = @job_listing.applied_jobs.find_by(talent_id: params[:applicant_id])
-    @talent = @applied_job.talent if @applied_job
-    @amount = @job_listing.salary.to_i / 2
-    @amount_in_dollars_in_cents = (@amount / 50 * 100)
-  end
+	def new
+		@job_listing = JobListing.find(params[:job_listing_id])
+		@applied_job = @job_listing.applied_jobs.find_by(talent_id: params[:applicant_id])
+		@talent = @applied_job.talent if @applied_job
+		@amount = @job_listing.salary.to_i / 2
+		@amount_in_dollars_in_cents = @amount * 100
+	end
 
-  def create
-    begin
-      @job_listing = JobListing.find(params[:job_listing_id])
-      @applied_job = @job_listing.applied_jobs.find(params[:applied_job_id])
-      @talent_id = @applied_job.talent_id
-      # @job_listing = current_user.job_listings.find(params[:job_listing_id])
-      # @applicants = @job_listing.applied_jobs.includes(:talent)
-      # @amount = @job_listing.salary.to_i / 2
+	def create
+		@job_listing = JobListing.find(params[:job_listing_id])
+		@applied_job = @job_listing.applied_jobs.find(params[:applied_job_id])
+		@talent_id = @applied_job.talent_id
+		@amount = @job_listing.salary.to_i / 2
+		@amount_in_dollars_in_cents = @amount * 100
+		
+		begin
+			if current_user.role_id == 3 
+				customer = Stripe::Customer.create({
+					email: params[:stripeEmail],
+					source: params[:stripeToken],
+				})
 
-      result = Transaction.send_payment(current_user, params[:job_listing_id], params[:applied_job_id])
+				charge = Stripe::Charge.create(
+					customer: customer.id,
+					amount: @amount_in_dollars_in_cents,
+					description: 'Rails Stripe customer',
+					currency: 'usd'
+				)
 
-      if result[:success]
-        redirect_to new_payment_path(job_listing_id: @job_listing.id, applicant_id: @talent_id), notice: result[:message]
-      else
-        redirect_to new_payment_path(job_listing_id: @job_listing.id, applicant_id: @talent_id), alert: result[:message]
-      end
+				result = Transaction.send_payment(current_user, params[:job_listing_id], params[:applied_job_id])
 
-      # ------ NOTES NI IAN
+				if result[:success]
+					redirect_to new_payment_path(job_listing_id: @job_listing.id, applicant_id: @talent_id), notice: result[:message]
+				else
+					redirect_to new_payment_path(job_listing_id: @job_listing.id, applicant_id: @talent_id), alert: result[:message]
+				end
+			
+			else
+				flash[:error] = 'You are not authorized to make this payment.'
+				redirect_to dashboard_path
+			end
 
-      # 4. Update yung wallet -> wallet + amount
-      # 5. Add transaction -> description: Downpayment, amount: amount (description: Downpayment, Completed)
-
-      # final: redirect sa review (optional yung review)
-
-      # if current_user.role_id == 3
-      #   customer = Stripe::Customer.create({
-      #     email: params[:stripeEmail],
-      #     source: params[:stripeToken],
-      #   })
-
-      #   charge = Stripe::Charge.create({
-      #     customer: customer.id,
-      #     amount: @amount.to_i,
-      #     description: 'Rails Stripe customer',
-      #     currency: 'usd',
-      #   })
-
-      #   flash[:notice] = 'Payment was successful'
-      #   @talent.update(wallet: @talent.wallet.to_i + @amount.to_i)
-      #   redirect_to job_listings_path, notice: 'Payment completed successfully.'
-      # else
-      #   flash[:error] = 'You are not authorized to make this payment.'
-      #   redirect_to root_path
-      # end
-
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_payment_path(job_listing_id: @job_listing.id)
-    rescue ActiveRecord::RecordNotFound
-      flash[:error] = 'Record not found.'
-      redirect_to new_payment_path(job_listing_id: @job_listing.id)
-    end
-  end
-
-  # The current client will send money to the talent through Stripe.
-        # The client doesn't need to manually input half of the salary; it will be calculated automatically.
-        # We could add another table for transactions
-
-    # def create
-    #     @applied_job = JobListing.find(2)
-    #     @amount = @applied_job.salary.to_i / 2
-
-    #     customer = Stripe::Customer.create({
-    #       email: params[:stripeEmail],
-    #       source: params[:stripeToken],
-    #     })
-
-    #     charge = Stripe::Charge.create({
-    #       customer: customer.id,
-    #       amount: @amount,
-    #       description: 'Rails Stripe customer',
-    #       currency: 'usd',
-    #     })
-
-    #     flash[:notice] = 'Payment was successful'
-    #     redirect_to new_payment_path
-
-    #   rescue Stripe::CardError => e
-    #   flash[:error] = e.message
-    #   redirect_to new_payment_path
-    # end
+			rescue Stripe::StripeError => e
+				flash[:error] = e.message
+				redirect_to new_payment_path(job_listing_id: @job_listing.id, applicant_id: @talent_id)
+		end
+	end
 end

548175f376ce24547983bf97e90e557bbef55428
dhanniela <tjmaureadhanniela@gmail.com>
2024-06-03 21:49:06 +0800

    fix: Resolve dropdown bugs and enhance styles for landing page, signups, and login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
-    def active_class(path)
-      current_page?(path) ? 'tab-link-active' : 'tab-link-inactive'
+    def active_class(path, active_class: 'tab-link-active')
+      current_page?(path) ? active_class : 'tab-link-inactive'
     end
   
     def aria_current(path)
       current_page?(path) ? 'page' : nil
     end
 end
-  
--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -1,5 +1,7 @@
 import { Application } from '@hotwired/stimulus'
 import Notification from '@stimulus-components/notification'
 
+console.log("Loading Notification Controller...")
+
 const application = Application.start()
 application.register('notification', Notification)

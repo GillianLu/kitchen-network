bf81a030343c455a21d20405598bca40e01fe6c9
dhanniela <tjmaureadhanniela@gmail.com>
2024-06-03 21:49:15 +0800

    fix: Resolve dropdown bugs and enhance styles for landing page, signups, and login

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -1,7 +1,5 @@
 import { Application } from '@hotwired/stimulus'
 import Notification from '@stimulus-components/notification'
 
-console.log("Loading Notification Controller...")
-
 const application = Application.start()
 application.register('notification', Notification)
